171fe0dbfecf0047356559e8ab98225d432ef580
Nathan Guimarães <nathangrs8@gmail.com>
2021-12-11 10:38:47 -0300

    First Commit

---
+++ assets/js/main.js
@@ -0,0 +1,56 @@
+/* === show menu === */
+const showMenu = (toggleId, navId) =>{
+    const toggle = document.getElementById(toggleId),
+    nav = document.getElementById(navId)
+
+    if(toggle && nav){
+        toggle.addEventListener('click', ()=>{
+            nav.classList.toggle('show')
+        })
+    }
+}
+showMenu('nav-toggle', 'nav-menu')
+
+/*  ===ACTIVE AND REMOVE MENU ===*/
+
+const navLink = document.querySelectorAll('.nav__link')
+
+function linkAction(){
+    //Active link
+    navLink.forEach( n => n.classList.remove('active'))
+    this.classList.add('active')
+
+    //remove menu mobile
+    const navMenu = document.getElementById('nav-menu')
+    navMenu.classList.remove('show')
+}
+
+navLink.forEach(n => n.addEventListener('click', linkAction))
+
+/* SCROLL REVEAL ANIMATION*/
+ScrollReveal({
+    origin:'top',
+    distance:'80px',
+    duration:2000,
+    reset:true
+});
+
+/*SCROLL HOME*/
+ScrollReveal().reveal('.home__title',{});
+ScrollReveal().reveal('.button',{delay:200});
+ScrollReveal().reveal('.home__img',{delay:400});
+ScrollReveal().reveal('.home__social-icon',{interval:200});
+
+/*SCROLL ABOUT*/
+ScrollReveal().reveal('.about__img',{});
+ScrollReveal().reveal('.about__subtitle',{delay:200});
+ScrollReveal().reveal('.about__text',{delay:400});
+
+/*SCROLL ABOUT*/
+ScrollReveal().reveal('.skills__subtitle',{});
+ScrollReveal().reveal('.skills__text',{delay:200});
+ScrollReveal().reveal('.skills__data',{interval:400});
+ScrollReveal().reveal('.skills__img',{delay:400});
+
+/*SCROLL WORK */
+ScrollReveal().reveal('.work__img',{interval:200});
